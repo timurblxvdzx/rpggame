bfc91ded2a265ee2c35ff85ba0845a57f8ee82d4
timurblxvdzx <47749323+timurblxvdzx@users.noreply.github.com>
2020-06-11 21:25:51 +0300

    first commit

---
+++ BLL/Getting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using DAL;
+using Models;
+
+namespace BLL
+{
+    public class Getting
+    {
+        Storage storage = new Storage();
+        public List<Armor> Armors { get; set; } = new List<Armor>();
+        public List<Weapon> Weapons { get; set; } = new List<Weapon>();
+        public List<Unit> Units { get; set; } = new List<Unit>();
+
+        public Getting()
+        {
+            Armors.AddRange(storage.Armors);
+            Weapons.AddRange(storage.Weapons);
+            Units.AddRange(storage.Units);
+        }
+
+        public void Create(Unit unit)
+        {
+            Units.Add(unit);
+        }
+
+    }
+}
